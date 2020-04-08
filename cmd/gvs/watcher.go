ab8e35a6fe2ff73762c04693087e5ba80b5ad196
daemonna <daemonna@localhost.localdomain>
2020-04-08 16:36:00 +0200

    tmp save

--- cmd/gvs/watcher.go
+++ cmd/gvs/watcher.go
@@ -5,6 +5,24 @@ import (
 	"time"
 )
 
+//Item in watchlist
+type Item struct {
+	path          string
+	last_seen     string //TODO: figure type
+	sleepseconds  int
+	sleeptreshold int
+}
+
+//Watchlist
+type Watchlist struct {
+	Items []Item
+}
+
+//WatchManager change sleep var depending on how often file changes
+func WatchManager() {
+
+}
+
 //WatchFile watches file
 func WatchFile(filePath string) error {
 	initialStat, err := os.Stat(filePath)
@@ -26,4 +44,9 @@ func WatchFile(filePath string) error {
 	}
 
 	return nil
+}
+
+//WatchDir watch directory
+func WatchDir(path string) {
+
 }
